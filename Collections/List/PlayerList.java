613e77ed391c74f098131546b4885744e55dd5a8
gurjot <gurjotsaini53@gmail.com>
2023-08-29 10:34:48 +0530

    learning collection list arraylist interface hierachy

--- Collections/List/PlayerList.java
+++ Collections/List/PlayerList.java
@@ -2,6 +2,7 @@ package Collections.List;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 public class PlayerList {
 
@@ -10,6 +11,9 @@ public class PlayerList {
      }
 
     public static void main(String[] args) {
+
+         /*
+
         Collection<String> player = new ArrayList<String>();
         player.add("Player_01");
         player.add("Player_02");
@@ -17,9 +21,51 @@ public class PlayerList {
         player.add("Player_04");
         player.add("Player_05");
 
+        System.out.println(player);
+
         for(String name : player)
         {
             System.out.println(name);
         }
+
+        */
+//-----------------------------------------------------------------------------------
+        /*
+
+        List<String> player = new ArrayList<String>();
+        player.add("Player_01");
+        player.add("Player_02");
+        player.add("Player_03");
+        player.add("Player_04");
+        player.add("Player_05");
+
+        System.out.println(player);
+
+        for(String name : player)
+        {
+            System.out.println(name);
+        }
+
+
+         */
+//---------------------------------------------------------------------------------------------------
+        
+
+
+
+        ArrayList<String> player = new ArrayList<String>();
+        player.add("Player_01");
+        player.add("Player_02");
+        player.add("Player_03");
+        player.add("Player_04");
+        player.add("Player_05");
+
+        System.out.println(player);
+
+        for(String name : player)
+        {
+            System.out.println(name);
+        }
+
     }
 }
