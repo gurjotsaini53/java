2c0ba19bab8d6609626a5800dbfc5a851ad23b01
gurjot <gurjotsaini53@gmail.com>
2023-08-31 10:41:25 +0530

    ques1

---
+++ Ques1.java
@@ -0,0 +1,18 @@
+public class Ques1 {
+
+    public static void main(String[] args) {
+        int n = 8 ;
+        while (n>0){
+        if (n==1)
+        {
+            System.out.print(n);
+            break;
+        }
+        else {
+            System.out.print(n + ",");
+        }
+        n--;
+    }
+    }
+
+}
