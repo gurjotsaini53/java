ccbe09e39bb78dfc2a569d4c0d3efee556ecbf54
gurjot <gurjotsaini53@gmail.com>
2023-09-04 10:52:44 +0530

    generic methods

---
+++ Generics/GenericMethods.java
@@ -0,0 +1,34 @@
+package Generics;
+
+public class GenericMethods <T>{
+
+    private T[] boxData;
+
+
+    public GenericMethods(T[] boxData)
+    {
+        this.boxData=boxData;
+    }
+
+    public <T> void showBoxData(T[] boxData)
+    {
+        for (T data:boxData)
+        {
+            System.out.println(data+ " ");
+        }
+        System.out.println();
+    }
+
+    public <T> void inspect(){
+        System.out.println("T : " + boxData.getClass().getSimpleName());
+    }
+
+    public static void main(String[] args) {
+
+        String [] item_name = {"Gurjot","Ansh","Coke","Fries","Wrap"};
+        GenericMethods<String> sObj = new GenericMethods<>(item_name);
+        sObj.showBoxData(item_name);
+        sObj.inspect();
+
+    }
+}
