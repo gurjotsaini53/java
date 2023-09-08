f46040b5f4f003bf646eb721806e15a7e884e30d
gurjot <gurjotsaini53@gmail.com>
2023-09-08 11:52:18 +0530

    adding local date time

---
+++ DateTime/LocalDateTimeExample.java
@@ -0,0 +1,14 @@
+package DateTime;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+public class LocalDateTimeExample {
+
+    public static void main(String[] args) {
+        LocalDateTime dt = LocalDateTime.now();
+        System.out.println(dt);
+
+    }
+}
--- DateTime/LocalTimeExample.java
+++ DateTime/LocalTimeExample.java
@@ -1,5 +1,7 @@
 package DateTime;
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
 public class LocalTimeExample {
 
     public static void main(String[] args) {
@@ -9,6 +11,17 @@ public class LocalTimeExample {
         System.out.println("Time : " + localTime);
         System.out.println("local hour: "  + localTime.getHour());
         System.out.println("local minutes : "  + localTime.getMinute());
+
+
+        // 12 hour clock
+        System.out.println(DateTimeFormatter.ofPattern("hh:mm").format(LocalTime.now()));
+
+        // 24 hour clock
+        System.out.println(DateTimeFormatter.ofPattern("HH:MM").format(LocalTime.now()));
+
+
+        // am or pm
+        System.out.println(DateTimeFormatter.ofPattern("hh:mm a").format(LocalTime.now()));
 
     }
 }
